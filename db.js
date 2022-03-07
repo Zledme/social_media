5488263cbeaa6703f2bbea563d3f1479a2b1793d
MridulBhatt <zled@pop-os.localdomain>
2022-03-07 12:30:48 +0530

    make it better

--- db.js
+++ db.js
@@ -10,8 +10,11 @@ require("dotenv").config();
 
 
 const pool = new Pool({
-     connectionString: process.env.DATABASE_URL,
-     ssl: true
+    user: "pfofxpkgtvyndd",
+    password: "8ab7b1173ba5a54a5e200bbc9a6bb876ae1e0d49e20945993c68fb839225c2e5",
+    host: "ec2-54-172-169-87.compute-1.amazonaws.com",
+    database: "d4us514oa8j7um",
+    port: 5432,
 });
 
 module.exports = pool;
