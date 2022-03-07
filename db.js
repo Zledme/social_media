066a3299c00929719b1f5c77485d08515d24dcc3
MridulBhatt <zled@pop-os.localdomain>
2022-03-07 10:43:58 +0530

    again

--- db.js
+++ db.js
@@ -1,17 +1,17 @@
 const Pool = require('pg').Pool;
 require("dotenv").config();
-const pool = new Pool({
-    user: "sammy",
-    password: "HeySammy",
-    host: "localhost",
-    database: "socialmedia",
-    port: 5432,
-});
-
-
 // const pool = new Pool({
-    
-//      connectionString: process.env.DATABASE_URL,
+//     user: "sammy",
+//     password: "HeySammy",
+//     host: "localhost",
+//     database: "socialmedia",
+//     port: 5432,
 // });
+
+
+const pool = new Pool({
+    
+     connectionString: process.env.DATABASE_URL,
+});
 
 module.exports = pool;
